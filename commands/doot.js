05e7fdff01020a4ff57c8efe45e964f3f0a74429
Caleb <emperor.pickles@gmail.com>
2021-09-16 18:14:38 -0400

    able to play audio file via message or interaction

--- commands/doot.js
+++ commands/doot.js
@@ -1,30 +1,60 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, VoiceConnectionStatus } = require('@discordjs/voice');
+const {
+    joinVoiceChannel,
+    createAudioPlayer,
+    createAudioResource,
+    entersState,
+    StreamType,
+    AudioPlayerStatus,
+    VoiceConnectionStatus,
+} = require('@discordjs/voice');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('doot')
         .setDescription('doot doot'),
     async execute(interaction) {
-        // create connection and join voice channel
-        const channel = interaction.member.voice.channel;
-        const connection = joinVoiceChannel({
-            channelId: channel.id,
-            guildId: channel.guild.id,
-            adapterCreator: channel.guild.voiceAdapterCreator,
-        });
-        
-        // create audio player
-        // const audioPlayer = createAudioPlayer();
-        // const resource = createAudioResource('/Users/caleb/Projects/mr-skeltal-2.0/doot.ogg');
+        // audio player setup
+        const player = createAudioPlayer();
+        function playSong() {
+            const resource = createAudioResource('./doot.ogg', { inputType: StreamType.Arbitrary });
+            player.play(resource);
+            return entersState(player, AudioPlayerStatus.Playing, 5e3);
+        }
 
-        connection.on(VoiceConnectionStatus.Ready, () => {
-            console.log('Voice connection is ready');
-            // audioPlayer.play(resource);
-            // subscribe to audio player and play audio on voice connection
-            // const subscription = connection.subscribe(audioPlayer);
-            setTimeout(() => connection.destroy(), 5000);
-        })
+        async function connectToChannel(channel) {
+            const connection = joinVoiceChannel({
+                channelId: channel.id,
+                guildId: channel.guild.id,
+                adapterCreator: channel.guild.voiceAdapterCreator,
+            });
+            try {
+                await entersState(connection, VoiceConnectionStatus.Ready, 30e3);
+                return connection;
+            } catch (err) {
+                connection.destroy();
+                throw err;
+            }
+        }
 
+        const channel = interaction.member?.voice.channel;
+        if (channel) {
+            try {
+                // create audio player
+                await playSong();
+                console.log('Ready to play');
+
+                // join voice channel and play audio file
+                const connection = await connectToChannel(channel);
+                interaction.reply('DOOT DOOT');
+                connection.subscribe(player);
+                await entersState(player, AudioPlayerStatus.Idle, 5e3);
+                connection.destroy();
+            } catch (err) {
+                console.error(err);
+            }
+        } else {
+            message.reply('Join a voice channel');
+        }
     },
 };
--- index.js
+++ index.js
@@ -1,23 +1,60 @@
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Collection, VoiceChannel, Intents } = require('discord.js');
+const {
+    joinVoiceChannel,
+    createAudioPlayer,
+    createAudioResource,
+    entersState,
+    StreamType,
+    AudioPlayerStatus,
+    VoiceConnectionStatus,
+} = require('@discordjs/voice');
+
 const fs = require('fs');
 require('dotenv').config();
 
+// client creation
 const token = process.env.BOT_TOKEN;
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_VOICE_STATES'] });
 
+// command registration
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.data.name, command);
 }
 
-client.once('ready', () => {
+// audio player setup
+const player = createAudioPlayer();
+
+function playSong() {
+    const resource = createAudioResource('./doot.mp3', { inputType: StreamType.Arbitrary });
+    player.play(resource);
+    return entersState(player, AudioPlayerStatus.Playing, 5e3);
+}
+
+async function connectToChannel(channel) {
+    const connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+    });
+    try {
+        await entersState(connection, VoiceConnectionStatus.Ready, 30e3);
+        return connection;
+    } catch (err) {
+        connection.destroy();
+        throw err;
+    }
+}
+
+// on client login
+client.on('ready', async () => {
     console.log(`Logged in as "${client.user.username}"`);
     console.log(`Connected to ${client.guilds.cache.size} server(s)`);
 });
 
+// on client interaction
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
@@ -29,6 +66,46 @@ client.on('interactionCreate', async interaction => {
     } catch (err) {
         console.error(err);
         await interaction.reply({ content: 'Error executing this command!', ephemeral: true });
+    }
+});
+
+// on client message
+client.on('messageCreate', async (message) => {
+    if (!message.guild) return;
+    if (!client.application?.owner) await client.application?.fetch();
+
+    // deploy new commands as interactions
+    if (message.content.toLowerCase() === '!deploy' && message.author.id === client.application?.owner?.id) {
+        let commands = [];
+        console.log('Deploying Commands:')
+        client.commands.forEach((v, k) => {
+            console.log(v.data);
+            commands.push(v.data);
+        });
+        await message.guild.commands.set(commands);
+        await message.reply({ content: 'Deployed', ephemeral: true });
+    }
+
+    // join voice channel and doot
+    if (message.content === '-join') {
+        const channel = message.member?.voice.channel;
+        if (channel) {
+            try {
+                // create audio player
+                await playSong();
+                console.log('Ready to play');
+
+                // join voice channel and play audio file
+                const connection = await connectToChannel(channel);
+                connection.subscribe(player);
+                await entersState(player, AudioPlayerStatus.Idle, 5e3);
+                connection.destroy();
+            } catch (err) {
+                console.error(err);
+            }
+        } else {
+            message.reply('Join a voice channel');
+        }
     }
 });
 
